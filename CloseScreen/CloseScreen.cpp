edbb5d5907b7eb8cbc54e0a109c52c879482d58a
pedoc <pedoc@qq.com>
2022-05-08 12:17:33 +0800

    添加项目文件。

---
+++ CloseScreen/CloseScreen.cpp
@@ -0,0 +1,11 @@
+﻿// CloseScreen.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <WinUser.h>
+
+int main()
+{
+    PostMessage((HWND)-1,0x112,0xF170,2);
+}
